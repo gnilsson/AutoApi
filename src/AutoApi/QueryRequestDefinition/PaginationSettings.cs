136fd47c05c60bce49a76ce3d574f3174e56b192
Gustav <g.nilsson1992@gmail.com>
2022-03-24 15:43:29 +0100

    clean up

--- src/AutoApi/QueryRequestDefinition/PaginationSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace AutoApi.QueryRequestDefinition;
-
-//public class PaginationSettings
-//{
-//    public int DefaultPageSize { get; init; } = 20;
-//    public int MaxPageSize { get; init; } = 50;
-//}
